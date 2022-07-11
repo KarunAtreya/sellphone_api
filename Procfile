b0460a0a0932dbcdd1e709999bc63a919fc87fcc
Karun Atreya <karunat1999@gmail.com>
2022-07-11 13:28:49 +0545

    Added req file

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn sellphone_api.wsgi
 release: python manage.py makemigrations --no input
+release: python manage.py collectstatic --no input
 release: python manage.py migrate --no input
--- sellphone_api/settings.py
+++ sellphone_api/settings.py
@@ -45,12 +45,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'sellphone_api.urls'
@@ -119,10 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL= 'api.UserProfile'
+
+django_heroku.settings(locals())
